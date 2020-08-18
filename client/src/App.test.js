414607f438412b28956f4fa3386f4d796710c4f6
Miri Michaelson <miriammichaelson@gmail.com>
2020-08-18 17:34:57 +0100

    some broken tests soz - api tests r hard

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import 'expect-puppeteer'
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
@@ -17,6 +18,29 @@ it("renders Account header", () => {
   const welcome = <h1>Welcome</h1>;
   expect(wrapper.contains(welcome)).toEqual(true);
 });
+
+
+describe('page', () => { 
+   beforeAll(async () => {
+    await page.goto('https://localhost:3000')
+     })
+     it('should display "API is working properly" text on page', async () => {
+        await expect(page).toMatch('API is working properly')  
+      })})
+
+// it('should display "google" text on page', async () => {
+//   const wrapper = shallow(<App />);
+//   await expect(wrapper).toMatch('API is working properly')  
+// });
+
+
+// it("renders our backend - API text", () => {
+//   const wrapper = shallow(<App />);
+//   const apiText = <p>API is working properly</p>;
+//   expect(wrapper.contains(apiText)).toEqual(true);
+// });
+
+
 
 // describe('Homepage working', () => {
 //   beforeAll(async () => {
