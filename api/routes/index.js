839d6c41c996162f619019353e765e6373e0d40c
Alessa <alessa_hardwick@live.co.uk>
2020-08-19 17:45:00 +0100

    attempted addition of API call

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var request = require("request");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,6 +11,15 @@ router.get('/', function(req, res, next) {
 router.post('/keyword',(req, res) => {
   var keyword = req.body.keyword;
   console.log(keyword);
+  request.get(`https://api.spotify.com/v1/search?q=%22${keyword}%22&type=playlist" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer`, true)
+  // console.log(res);
+  request.on = function () {
+    var data = JSON.parse(this.response)
+    console.log(data)
+    // "playlists": {
+    // "items": [
+    //   "id": "THIS PLAYLIST_ID"
+  }
   res.redirect('/')
 });
 
