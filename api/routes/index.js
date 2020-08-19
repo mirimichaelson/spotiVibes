17e696e437db6720c0c91e7793259a8ea4a15497
Miri Michaelson <miriammichaelson@gmail.com>
2020-08-19 18:07:42 +0100

    made changes to our backend to try and link up spotify api. Currently working on receiving data back from API. to be continued tomorrow

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,19 +8,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/keyword',(req, res) => {
+router.post('/keyword', function(req, res) {
   var keyword = req.body.keyword;
   console.log(keyword);
-  request.get(`https://api.spotify.com/v1/search?q=%22${keyword}%22&type=playlist" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer`, true)
-  // console.log(res);
-  request.on = function () {
-    var data = JSON.parse(this.response)
-    console.log(data)
-    // "playlists": {
-    // "items": [
-    //   "id": "THIS PLAYLIST_ID"
-  }
-  res.redirect('/')
-});
+   request(`https://api.spotify.com/v1/search?q=%22${keyword}%22&type=playlist&limit=1" -H "Accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer BQC9uUE1DbqIfwQFyVeULezJZ5AbhXn_E8sBFlbAVIXpe4gb0wxBN03OAfLxHDVg6RpNihG_jXvsDtjEc3uM-sr74whKC3QKrvP4zFBacmXHB74Rx8Zh5RcqjrlsKjutmJ6SXw38iA7p3ygG`, function(error, response, body) {
+    console.log("Hi im not in the if statemnet (i am in the request tho)")
+    // if (!error && response.statusCode == 200) {
+      // writing the response to a file named data.html
+      var data = JSON.parse(this.response);
+      console.log(data);
+      // }
+    
+  });
+ });
+//   // console.log(res);
+//   request.on = function () {
+//     var data = JSON.parse(this.response)
+//     console.log(data)
+//     // "playlists": {
+//     // "items": [
+//     //   "id": "THIS PLAYLIST_ID"
+//   }
+//   res.redirect('/')
+
+  
 
 module.exports = router;
