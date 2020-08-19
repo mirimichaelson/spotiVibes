23a0dbac82bcd5d8bad7426d841b5012d2313477
Miri Michaelson <miriammichaelson@gmail.com>
2020-08-19 12:43:00 +0100

    added package.json to client

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,54 +1,54 @@
-// import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from './App';
-// import 'expect-puppeteer'
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import 'expect-puppeteer'
 
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
-// it("renders without crashing", () => {
-//   shallow(<App />);
-// });
+it("renders without crashing", () => {
+  shallow(<App />);
+});
 
-// it("renders Account header", () => {
+it("renders Account header", () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h1>Welcome</h1>;
+  expect(wrapper.contains(welcome)).toEqual(true);
+});
+
+
+describe('page', () => { 
+   beforeAll(async () => {
+    await page.goto('https://localhost:3000')
+     })
+     it('should display "API is working properly" text on page', async () => {
+        await expect(page).toMatch('API is working properly')  
+      })})
+
+// it('should display "google" text on page', async () => {
 //   const wrapper = shallow(<App />);
-//   const welcome = <h1>Welcome</h1>;
-//   expect(wrapper.contains(welcome)).toEqual(true);
+//   await expect(wrapper).toMatch('API is working properly')  
 // });
 
 
-// describe('page', () => { 
-//    beforeAll(async () => {
-//     await page.goto('https://localhost:3000')
-//      })
-//      it('should display "API is working properly" text on page', async () => {
-//         await expect(page).toMatch('API is working properly')  
-//       })})
-
-// // it('should display "google" text on page', async () => {
-// //   const wrapper = shallow(<App />);
-// //   await expect(wrapper).toMatch('API is working properly')  
-// // });
-
-
-// // it("renders our backend - API text", () => {
-// //   const wrapper = shallow(<App />);
-// //   const apiText = <p>API is working properly</p>;
-// //   expect(wrapper.contains(apiText)).toEqual(true);
-// // });
+// it("renders our backend - API text", () => {
+//   const wrapper = shallow(<App />);
+//   const apiText = <p>API is working properly</p>;
+//   expect(wrapper.contains(apiText)).toEqual(true);
+// });
 
 
 
-// // describe('Homepage working', () => {
-// //   beforeAll(async () => {
-// //     await page.goto('/');
-// //   });
+// describe('Homepage working', () => {
+//   beforeAll(async () => {
+//     await page.goto('/');
+//   });
 
-// //   it('should display the homepage correctly', async () => {
-// //     await expect(page.body()).toInclude('');
-// //   });
+//   it('should display the homepage correctly', async () => {
+//     await expect(page.body()).toInclude('');
+//   });
 
-// // })
+// })
